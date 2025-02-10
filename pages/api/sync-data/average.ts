ea05eea71753806a7b7fa26f158f070f35c74614
Biu <mac@192.168.1.90.non-exists.ptr.local>
2025-02-10 23:49:00 +0700

    f

--- pages/api/sync-data/average.ts
+++ pages/api/sync-data/average.ts
@@ -6,36 +6,41 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
- 
-  if (req.method === "POST") {
-    const apiKey = req.body.apiKey;
-
-    // if (!apiKey) {
-    //   return res.status(400).send({});
-    // }
-
     try {
-      const result = await createOrUpdate()
-      return res.status(200).send(result);
-    } catch (error) {
-      return res.status(400).send({});
-    }
-  }
-
-  if (req.method === "PATCH") {
-    const apiKey = req.body.apiKey;
-    const index = req.body.index
-
-    if (!apiKey) {
+        const result = await createOrUpdate()
+        return res.status(200).send(result);
+      } catch (error) {
         return res.status(400).send({});
       }
-    try {
-      const result = await updateElementByIndex(index)
-      return res.status(200).send(result);
-    } catch (error) {
-      return res.status(400).send({});
-    }
-  }
+//   if (req.method === "POST") {
+//     const apiKey = req.body.apiKey;
 
-  return res.status(400).send({});
+//     // if (!apiKey) {
+//     //   return res.status(400).send({});
+//     // }
+
+//     try {
+//       const result = await createOrUpdate()
+//       return res.status(200).send(result);
+//     } catch (error) {
+//       return res.status(400).send({});
+//     }
+//   }
+
+//   if (req.method === "PATCH") {
+//     const apiKey = req.body.apiKey;
+//     const index = req.body.index
+
+//     if (!apiKey) {
+//         return res.status(400).send({});
+//       }
+//     try {
+//       const result = await updateElementByIndex(index)
+//       return res.status(200).send(result);
+//     } catch (error) {
+//       return res.status(400).send({});
+//     }
+//   }
+
+//   return res.status(400).send({});
 }
