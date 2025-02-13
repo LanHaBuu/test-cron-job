cfd4f4e248bc92e102f399088c4941a635464009
Biu <mac@Biu.local>
2025-02-13 11:19:54 +0700

    f

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,23 +2,15 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-    // const allowedOrigin = "https://test-cron-job-mu.vercel.app";
-    // const origin = req.headers.get("origin") || req.headers.get("referer");
+    const allowedOrigin = "https://test-cron-job-mu.vercel.app";
+    const origin = req.headers.get("origin") || req.headers.get("referer");
 
-    // // Allow only requests from your website
-    // if (!origin || !origin.startsWith(allowedOrigin)) {
-    //     return NextResponse.json({ error: "Forbidden" }, { status: 403 });
-    // }
-
-    // return NextResponse.next();
-    const allowedOrigin = "https://test-cron.vercel.app"; // Thay bằng domain của bạn
-    const origin = req.headers.get("origin");
-  
-    if (origin && origin !== allowedOrigin) {
-      return new Response("Forbidden", { status: 403 });
+    // Allow only requests from your website
+    if (!origin || !origin.startsWith(allowedOrigin)) {
+        return NextResponse.json({ error: "Forbidden" }, { status: 403 });
     }
-  
-    return new Response("OK", { status: 200 });
+
+    return NextResponse.next();
 }
 
 // Apply middleware only to the API route
